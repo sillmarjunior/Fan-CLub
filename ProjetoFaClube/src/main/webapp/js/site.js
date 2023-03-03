9f8e7c0aca8f729c09126e0ea33185cfc3e2121a
SILMAR CARDOSO DA SILVA JUNIOR <silmar_silva-junior@estudante.sesisenai.org.br>
2023-03-02 21:54:54 -0300

    Altera Alteração na ordem dos inputs em descricao.html Alteração na ordem das validações dos inputs em site.js Validação do campo checkbox em andamento

--- ProjetoFaClube/src/main/webapp/js/site.js
+++ ProjetoFaClube/src/main/webapp/js/site.js
@@ -3,10 +3,6 @@ function validaFaleConosco() {
 		alert("Preencha o campo Nome.");
 		document.frmfaleconosco.txtnome.focus();
 		return false;
-	} else if (document.frmfaleconosco.genero.value == "") {
-		alert("Selecione um dos campos, Gênero");
-		document.frmfaleconosco.genero.focus();
-		return false;
 	} else if (document.frmfaleconosco.nascimento.value == "") {
 		alert("Preencha o campo, Nascimento.");
 		document.frmfaleconosco.nascimento.focus();
@@ -22,6 +18,14 @@ function validaFaleConosco() {
 	} else if (document.frmfaleconosco.txtsenha.value == "") {
 		alert("Preencha o campo, Senha.");
 		document.frmfaleconosco.txtsenha.focus();
+		return false;
+	}else if (document.frmfaleconosco.genero.value == "") {
+		alert("Selecione um dos campos, Gênero");
+		document.frmfaleconosco.genero.focus();
+		return false;
+	}else if (document.frmfaleconosco.caixa.checked) {
+		alert("Selecione o campo, Desejo participar...");
+		document.frmfaleconosco.caixa.focus();
 		return false;
 	}
 	return true;
