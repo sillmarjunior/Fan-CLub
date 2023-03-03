c731a1f415d9cf6715afd8f6b20994c50a4d4f93
SILMAR CARDOSO DA SILVA JUNIOR <silmar_silva-junior@estudante.sesisenai.org.br>
2023-03-02 21:04:22 -0300

    Adiciona Adicionado projeto fan club em andamento

---
+++ ProjetoFaClube/src/main/webapp/js/site.js
@@ -0,0 +1,28 @@
+function validaFaleConosco() {
+	if (document.frmfaleconosco.txtnome.value == "") {
+		alert("Preencha o campo Nome.");
+		document.frmfaleconosco.txtnome.focus();
+		return false;
+	} else if (document.frmfaleconosco.genero.value == "") {
+		alert("Selecione um dos campos, Gênero");
+		document.frmfaleconosco.genero.focus();
+		return false;
+	} else if (document.frmfaleconosco.nascimento.value == "") {
+		alert("Preencha o campo, Nascimento.");
+		document.frmfaleconosco.nascimento.focus();
+		return false;
+	} else if (document.frmfaleconosco.txtemail.value == "") {
+		alert("Preencha o campo E-mail.");
+		document.frmfaleconosco.txtemail.focus();
+		return false;
+	} else if (document.frmfaleconosco.txtfone.value == "") {
+		alert("Preencha o campo, Telefone.");
+		document.frmfaleconosco.txtfone.focus();
+		return false;
+	} else if (document.frmfaleconosco.txtsenha.value == "") {
+		alert("Preencha o campo, Senha.");
+		document.frmfaleconosco.txtsenha.focus();
+		return false;
+	}
+	return true;
+}
